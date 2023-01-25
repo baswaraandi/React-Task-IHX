5224fea51188a221d1993b6aab1ec65889d58363
azizahcahya <zellchan07@gmail.com>
2023-01-26 00:01:11 +0700

    add: counter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -84,6 +84,24 @@ function App() {
     clearTimer(getDeadTime());
   };
 
+  // counter
+  const [counter, setCounter] = useState(0);
+
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+
+  const decrease = () => {
+    if (counter > 0){
+      setCounter(count => count - 1);
+    }
+  };
+
+  const reset = () => {
+    setCounter(0);
+  };
+  //counter
+
   return (
     <div className="App">
       <header className="App-header">
@@ -118,6 +136,18 @@ function App() {
         <List items={listItem} />
       </div>
       {/* END TODO LIST */}
+
+      {/* counter */}
+      <div className="counter">
+        <h1> React Counter </h1>
+        <span className="counter__output">{counter}</span>
+        <span className="btn__container">
+          <button className="control__btn" onClick={increase}>+</button>
+          <button className="control__btn" onClick={decrease}>-</button>
+          <button className="reset" onClick={reset}>Reset</button>
+        </span>
+      </div>
+      {/* counter */}
     </div>
   );
 }
